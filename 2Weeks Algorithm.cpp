9637f85e237f3e5e5721a78fd18dfcf74e9d3c76
MATTTTJ <hoon3022@naver.com>
2023-10-19 21:30:28 +0900

    10-19 study

--- 2Weeks Algorithm.cpp
+++ 2Weeks Algorithm.cpp
@@ -88,45 +88,89 @@ using namespace std;
 //	}
 //}
 
-const int V = 10;
-vector<int> adj[V];
-int visited[V];
-void go(int idx)
+//const int V = 10;
+//vector<int> adj[V];
+//int visited[V];
+//void go(int idx)
+//{
+//	cout << idx << '\n';
+//	visited[idx] = 1;
+//	for (int there : adj[idx])
+//	{
+//		if (visited[there])
+//			continue;
+//		go(there);
+//	}
+//	return;
+//}
+//
+//int main()
+//{
+//	adj[1].push_back(2);
+//	adj[2].push_back(1);
+//
+//	adj[1].push_back(3);
+//	adj[3].push_back(1);
+//
+//	adj[3].push_back(4);
+//	adj[4].push_back(3);
+//
+//	for (int i = 0; i < V; i++)
+//	{
+//		if (adj[i].size() && visited[i] == 0)
+//			go(i);
+//	}
+//}
+//
+//
+
+//const int dy[] = { -1, 0, 1, 0 };
+//const int dx[] = { 0, 1, 0, - 1 };
+//
+//int main()
+//{
+//	int y = 0, x = 0;
+//	for (int i = 0; i < 4; i++)
+//	{
+//		int ny = y + dy[i];
+//		int nx = x + dx[i];
+//		cout << ny << " : " << nx << '\n';
+//	}
+//
+//	return 0;
+//}
+
+const int n = 3;
+int a[n][n], visited[n][n];
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+
+void go(int y, int x)
 {
-	cout << idx << '\n';
-	visited[idx] = 1;
-	for (int there : adj[idx])
+	visited[y][x] = 1;
+	cout << y << " : " << x << "\n";
+	for (int i = 0; i < 4; i++)
 	{
-		if (visited[there])
-			continue;
-		go(there);
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+		if (a[ny][nx] == 0) continue;
+		if (visited[ny][nx]) continue;
+		go(ny, nx);
 	}
 	return;
 }
 
 int main()
 {
-	adj[1].push_back(2);
-	adj[2].push_back(1);
-
-	adj[1].push_back(3);
-	adj[3].push_back(1);
-
-	adj[3].push_back(4);
-	adj[4].push_back(3);
-
-	for (int i = 0; i < V; i++)
+	for (int i = 0; i < n; i++)
 	{
-		if (adj[i].size() && visited[i] == 0)
-			go(i);
+		for (int j = 0; j < n; j++)
+		{
+			cin >> a[i][j];
+		}
+		go(0, 0);
 	}
 }
-
-
-
-
-
-
-
 
 
