fba07cd347df3d03520d8c7948e1bdbe58435d11
MATTTTJ <hoon3022@naver.com>
2023-10-20 21:56:57 +0900

    1012 clear

--- 2Weeks Algorithm.cpp
+++ 2Weeks Algorithm.cpp
@@ -11,43 +11,72 @@
 
 using namespace std;
 
-const int max_n = 104;
-int dy[4] = { -1, 0, 1, 0 }, dx[4] = { 0, 1, 0, -1 };
-int n, m, a[max_n][max_n], visited[max_n][max_n], y, x;
+// algorithm 1012
+
+int dy[4]{ -1, 0, 1, 0 }, dx[4]{ 0,1,0,-1 };
+int m, n, k, y, x, ny, nx, t, ret;
+int a[51][51];
+bool visited[51][51];
+
+void dfs(int y, int x)
+{
+	visited[y][x] = 1;
+	for(int i = 0; i < 4; i++)
+	{
+		ny = y + dy[i];
+		nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+		if(a[ny][nx] == 1 && !visited[ny][nx])
+		{
+			dfs(ny, nx);
+		}
+	}
+	return;
+}
 
 int main()
 {
-	scanf("%d %d", &n, &m);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> t;
 
-	for(int i =0; i< n; i++)
+	queue<int> count;
+
+	while (t--)
 	{
-		for(int j = 0; j <m; j++)
+
+		fill(&a[0][0], &a[0][0] + 51 * 51, 0);
+		fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
+		ret = 0;
+		cin >> m >> n >> k;
+
+		for (int i = 0; i < k; i++)
 		{
-			scanf("%1d", &a[i][j]);
+			cin >> x >> y;
+			a[y][x] = 1;
 		}
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (a[i][j] == 1 && !visited[i][j])
+				{
+					dfs(i, j);
+					ret++;
+				}
+			}
+		}
+		count.push(ret);
 	}
 
-	queue<pair<int, int>> q;
-	visited[0][0] = 1;
-	q.push({ 0, 0 });
-
-	while(q.size())
+	while(count.size())
 	{
-		tie(y, x) = q.front();
-		q.pop();
-
-		for(int i =0; i < 4; i++)
-		{
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-
-			if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0) continue;
-			if (visited[ny][nx])  continue;
-			visited[ny][nx] = visited[y][x] + 1;
-			q.push({ ny, nx });
-		}
+		cout << count.front() << "\n";
+		count.pop();
 	}
+	
 
-	printf("%d", visited[n - 1][m - 1]);
 	return 0;
 }
