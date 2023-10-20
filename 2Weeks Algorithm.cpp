c31e913434b611dea0e66fcb0ea2055a18539ab1
MATTTTJ <hoon3022@naver.com>
2023-10-20 21:06:31 +0900

    2178 BFS 복습

--- 2Weeks Algorithm.cpp
+++ 2Weeks Algorithm.cpp
@@ -11,56 +11,43 @@
 
 using namespace std;
 
-// algorithm 2178 
-
 const int max_n = 104;
-
-int dy[4] = { -1, 0, 1, 0 };
-int dx[4] = { 0, 1, 0, -1 };
-
+int dy[4] = { -1, 0, 1, 0 }, dx[4] = { 0, 1, 0, -1 };
 int n, m, a[max_n][max_n], visited[max_n][max_n], y, x;
 
-int main ()
+int main()
 {
 	scanf("%d %d", &n, &m);
 
-	for(int i = 0; i < n; i ++)
+	for(int i =0; i< n; i++)
 	{
-		for(int j = 0; j <m; j ++)
+		for(int j = 0; j <m; j++)
 		{
-			// N x M 배열을 입력받기 위함
 			scanf("%1d", &a[i][j]);
 		}
 	}
-	// y,x 좌표가 주어지기 때문에 두개를 담을 수 있는 페어
+
 	queue<pair<int, int>> q;
-	// 방문처리 후 q 푸쉬
 	visited[0][0] = 1;
-	q.push({ 0,0 });
+	q.push({ 0, 0 });
 
 	while(q.size())
 	{
-		//y,x 뽑아내기 
 		tie(y, x) = q.front();
 		q.pop();
 
-		for(int i =0; i < 4; i ++)
+		for(int i =0; i < 4; i++)
 		{
-			// 4방향 탐색하기 
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			// 마지막 예외는 건널 수 없는 조건 (바다)
-			// 오버플로우 예외처리 먼저 하고 진행해야 함
+
 			if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0) continue;
-			// 방문처리 확인
-			if (visited[ny][nx]) continue;
+			if (visited[ny][nx])  continue;
 			visited[ny][nx] = visited[y][x] + 1;
-			// 최단거리 구하기 완료
 			q.push({ ny, nx });
 		}
 	}
 
 	printf("%d", visited[n - 1][m - 1]);
-
 	return 0;
 }
