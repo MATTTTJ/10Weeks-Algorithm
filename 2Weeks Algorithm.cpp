819058a11f2998d20a671df6045ce55251bfe279
MATTTTJ <hoon3022@naver.com>
2023-10-24 19:26:37 +0900

    solved.ac 2910 clear

--- 2Weeks Algorithm.cpp
+++ 2Weeks Algorithm.cpp
@@ -11,35 +11,50 @@
 
 using namespace std;
 
-// algorithm 2828
+// algorithm 2910 (custom operator)
 
-int n, m, j, l, r, temp, ret;
+int n, c, a[1004];
+vector<pair<int, int>> v;
+map<int, int> mp, mp_first;
+
+bool cmp(pair<int,int> a, pair<int,int> b)
+{
+	// 같은 수를 카운팅하고 대소 비교를 통해 정렬하기 위함
+	if(a.first == b.first)
+	{
+		return mp_first[a.second] < mp_first[b.second];
+	}
+	return a.first > b.first;
+}
+
 int main()
 {
-	cin >> n >> m >> j;
-	l = 1;
-	for(int i = 0; i < j; i++)
-	{
-		r = l + m -1;
-		cin >> temp;
+	cin >> n >> c;
 
-		if (temp >= l && temp <= r) continue;
-		else
+	for(int i =0; i < n; i++)
+	{
+		cin >> a[i];
+		mp[a[i]]++;
+		// 숫자를 인덱스로 사용하고 같은 인덱스가 있는지 체크해서 카운팅을 늘리는 방식
+		if (mp_first[a[i]] == 0)
+			mp_first[a[i]] = i + 1;
+	}
+	// 키 값에 카운팅이 들어가기 때문에 이를 기반으로 정렬하기 위함
+	for(auto it : mp)
+	{
+		v.push_back({ it.second, it.first });
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for(auto it : v)
+	{
+		for(int j = 0; j < it.first; j++)
 		{
-			if(temp < l)
-			{
-				ret += (l - temp);
-				l = temp;
-			}
-			else
-			{
-				l += (temp - r);
-				ret += (temp - r);
-			}
+			cout << it.second << " ";
 		}
 	}
 
-	cout << ret << "\n";
 	return 0;
 }
 
