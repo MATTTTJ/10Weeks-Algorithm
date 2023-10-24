305d0a475b3afa524656a5aca55e559d5ca4c231
MATTTTJ <hoon3022@naver.com>
2023-10-24 15:27:41 +0900

    solved.ac 2583 clear

--- 2Weeks Algorithm.cpp
+++ 2Weeks Algorithm.cpp
@@ -11,63 +11,64 @@
 
 using namespace std;
 
-// algorithm 2468
+#define y1 aaaa
 
-int a[101][101], e[101][101], visited[101][101], n, temp, ret = 1;
-int dy[4]{ -1,0,1,0 }, dx[4]{ 0,1,0,-1 };
+// algorithm 2583
 
-void dfs(int y, int x, int d)
+int a[104][104], m, n, k, x1, x2, y1, y2, visited[104][104];
+const int dy[4] = { -1, 0, 1, 0 };
+const int dx[4] = { 0, 1, 0, -1 };
+vector<int> ret;
+int dfs(int y, int x)
 {
 	visited[y][x] = 1;
+	int ret = 1;
 	for(int i = 0; i < 4; i++)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
-		if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-		if (!visited[ny][nx] && a[ny][nx] > d)
-			dfs(ny, nx, d);
+		if(ny < 0 || ny >= m || nx < 0 || nx >= n || visited[ny][nx] == 1) continue;
+
+		if (a[ny][nx] == 1) continue;
+
+		ret += dfs(ny, nx);
 	}
-
-	return;
+	return ret;
 }
-
 
 int main()
 {
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin >> m >> n >> k;
 
-	cin >> n;
+	for(int i =0; i < k; i++)
+	{
+		cin >> x1 >> y1 >> x2 >> y2;
+		for(int x = x1; x < x2; x++)
+		{
+			for(int y = y1; y < y2; y++)
+			{
+				a[y][x] = 1;
+			}
+		}
+	}
 
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < m; i++)
 	{
 		for(int j = 0; j < n; j++)
 		{
-			cin >> a[i][j];
-		}
-	}
-
-	for(int d = 1; d < 101; d++)
-	{
-		fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
-
-		int cnt = 0;
-
-		for(int i =0; i < n; i++)
-		{
-			for(int j =0; j < n; j++)
+			if(a[i][j] != 1 & visited[i][j] ==0)
 			{
-				if (a[i][j] > d && !visited[i][j])
-				{
-					dfs(i, j, d);
-					cnt++;
-				}
+				ret.push_back(dfs(i, j));
 			}
 		}
-		ret = max(ret, cnt);
 	}
 
-	cout << ret << '\n';
+	sort(ret.begin(), ret.end());
+
+	cout << ret.size() << "\n";
+
+	for (int a : ret) cout << a << " ";
+
 	return 0;
 }
