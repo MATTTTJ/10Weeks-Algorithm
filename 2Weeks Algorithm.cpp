10279bb990b23cf549a09019b33b208b64ed537e
MATTTTJ <hoon3022@naver.com>
2023-10-31 15:41:02 +0900

    solved.ac 10709 clear

--- 2Weeks Algorithm.cpp
+++ 2Weeks Algorithm.cpp
@@ -11,58 +11,50 @@
 
 using namespace std;
 
-// algorithm 2870
+// algorithm 10709
 
-int n;
-vector<string> v;
-string s, ret;
-
-void go()
-{
-	//0지우기
-	while(true)
-	{
-		if (ret.size() && ret.front() == '0')
-			ret.erase(ret.begin());
-		else
-			break;
-	}
-	if (ret.size() == 0)
-		ret = "0";
-	v.push_back(ret);
-	ret = "";
-}
-// 문자열인 숫자를 비교하기 위함
-bool cmp(string a, string b)
-{
-	if (a.size() == b.size())
-		return a < b;
-
-	return a.size() < b.size();
-}
+int n, m, a[104][104];
+string s;
 
 int main()
 {
-	cin >> n;
+	cin >> n >> m;
+	for(int i =0; i <n; i++)
+	{
+		cin >> s;
+		for(int j = 0; j < m; j++)
+		{
+			if (s[j] == '.')
+				a[i][j] = -1;
+			else
+				a[i][j] = 0;
+		}
+	}
 
 	for(int i =0; i < n; i++)
 	{
-		cin >> s;
-		ret = "";
-		for(int j =0; j < s.size(); j++)
+		for(int j =0; j < m; j++)
 		{
-			if (s[j] < 65)
-				ret += s[j];
-			else if (ret.size())
-				go();
+			if(a[i][j] == 0)
+			{
+				int cnt = 1;
+
+				while(a[i][j+1] == -1)
+				{
+					a[i][j + 1] = cnt++;
+					j++;
+				}
+			}
 		}
-		if (ret.size())
-			go();
 	}
-	sort(v.begin(), v.end(), cmp);
-	for (string i : v)
-		cout << i << "\n";
 
-
+	for(int i =0; i < n; i++)
+	{
+		for(int j =0; j < m; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}
 	return 0;
 }
