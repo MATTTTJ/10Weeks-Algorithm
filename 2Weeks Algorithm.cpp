dc8498b5a479b88944f3d286e8afce14c84b9fb9
MATTTTJ <hoon3022@naver.com>
2023-11-07 19:31:27 +0900

    solved.ac 2636 clear

--- 2Weeks Algorithm.cpp
+++ 2Weeks Algorithm.cpp
@@ -14,86 +14,77 @@
 using namespace std;
 typedef long long ll;
 
-// algorithm 14502
+// algorithm 2636
 
-int a[10][10], visited[10][10], n, m, ret;
-vector<pair<int, int>> virusList, wallList;
+int a[104][104], visited[104][104];
+int dy[] = { -1, 0, 1,0 }, dx[] = { 0,1,0, -1 };
+int n, m, cnt, cnt2;
+vector<pair<int, int>> v;
 
-const int dy[] = { -1, 0, 1, 0 };
-const int dx[] = { 0, 1, 0, -1 };
-
-void dfs(int y, int x)
+void go(int y, int x)
 {
-	for(int i = 0; i< 4; i ++)
+	visited[y][x] = 1;
+	if(a[y][x] ==1)
+	{
+		// 치즈인경우 더이상 방문하지 않고 벡터에 푸시
+		v.push_back({ y,x });
+		return;
+	}
+	for(int i =0;i <4; i++)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || a[ny][nx] == 1)
+		if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx])
 			continue;
-		visited[ny][nx] = 1;
-		dfs(ny, nx);
+		go(ny, nx);
 	}
 	return;
 }
 
-// 벽을 세우고 세운 벽을 기반으로 바이러스가 퍼지고, 이 결과를 토대로 안전영역을 체크하는 함수
-int solve()
-{
-	// 경우의 수 마다 초기화해주고 dfs를 돌려야 한다. 바이러스가 퍼져나가는 것을 표현
-	fill(&visited[0][0], &visited[0][0] + 10 * 10, 0);
-	for (pair<int, int> b : virusList)
-	{
-		visited[b.first][b.second] = 1;
-		dfs(b.first, b.second);
-	}
-
-	int cnt = 0;
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < m; j++)
-		{
-			if (a[i][j] == 0 && !visited[i][j])
-				cnt++;
-		}
-	}
-	return cnt;
-}
 
 int main()
 {
 	CODINGTEST
-		cin >> n >> m;
+	cin >> n >> m;
 
-	for(int i =0; i< n; i++)
+	for(int i =0; i < n; i++)
 	{
 		for(int j = 0; j < m; j++)
 		{
 			cin >> a[i][j];
-			if (a[i][j] == 2)
-				virusList.push_back({ i,j });
-			// 벽을 세울 수 있는 지점을 골라내서 좌표를 담는 작업
-			if (a[i][j] == 0)
-				wallList.push_back({ i,j });
 		}
 	}
 
-	for(int i =0; i < wallList.size();i++)
+
+	while(true)
 	{
-		for(int j =0; j<i; j++)
+		cnt2 = 0;
+		fill(&visited[0][0], &visited[0][0] + 104 * 104, 0);
+		v.clear();
+		go(0, 0);
+		// 치즈가 녹기 전의 사이즈
+		cnt2 = v.size();
+		// 치즈를 녹인다.
+		for(pair<int, int> b : v)
 		{
-			for(int k = 0; k < j; k++)
-			{
-				a[wallList[i].first][wallList[i].second] = 1;
-				a[wallList[j].first][wallList[j].second] = 1;
-				a[wallList[k].first][wallList[k].second] = 1;
-				ret = max(ret, solve());
-				a[wallList[i].first][wallList[i].second] = 0;
-				a[wallList[j].first][wallList[j].second] = 0;
-				a[wallList[k].first][wallList[k].second] = 0;
-			}
-
+			cnt2++;
+			a[b.first][b.second] = 0;
 		}
+		// 치즈가 다 녹았는지 체크
+		bool flag = 0;
+		for(int i =0; i<n; i++)
+		{
+			for(int j =0; j <m; j++)
+			{
+				if (a[i][j] != 0)
+					flag = 1;
+			}
+		}
+		cnt++;
+		if (!flag)
+			break;
 	}
-	cout << ret << "\n";
+	cout << cnt << "\n" << cnt2 << '\n';
+
 	return 0;
 }
